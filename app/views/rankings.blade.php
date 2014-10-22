dade66121bcfdce04d8cb57e035323f9d02f3edd
Nicky Semenza <14nicholasse@gmail.com>
2014-10-22 19:06:34 -0400

    tracking

--- app/views/rankings.blade.php
+++ app/views/rankings.blade.php
@@ -10,6 +10,16 @@
     {{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css'); }}
 </head>
 <body>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-26898313-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <div class="container" style="margin-top:20px;">
 	<h1>Rankings</h1>
 	<table class="table table-striped table-bordered">
